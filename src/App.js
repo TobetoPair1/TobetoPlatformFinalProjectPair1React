4c4f0af4d9565313defae1153df08608844a1d29
Atilla <atillaagungorr@gmail.com>
2023-12-22 19:03:54 +0300

    Visual updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import CheckUser from './Services/UserService';
+import logo from "./logo.svg";
+
+import "./App.css";
+import { useEffect, useState } from "react";
+import CheckUser from "./Services/UserService";
 
 function App() {
   // useEffect(()=>{
   //   console.log(CheckUser("eposta","şifre").then(result=>result.data.items));
   // },[]);
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const [user,setUser]=useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
 
-  useEffect(()=>{    
+  useEffect(() => {
     console.log("Doğrulanan Kullanıcı");
     console.log(user);
-  },[user]);
+  }, [user]);
   return (
     <div className="App">
+      <label class="switch">
+      <input type="checkbox"/>
+      <span class="slider round"></span>
+      </label>
       <header>
-        <img src={logo} className="App-logo" alt="logo" width={100} />        
+        <img src={logo} className="App-logo" alt="logo" width={100} />
       </header>
       <main>
-        <input type='text' placeholder='E-Mail' onChange={email=>setEmail(email.target.value)}/><br/>
-        <input type='text' placeholder='Password' onChange={password=>setPassword(password.target.value)}/><br/>
-        <button onClick={()=>{
-
-          setUser(CheckUser(email,password).then(result=>result.data))
-          
-          }}>Giriş Yap</button>
+        <input
+          className="email"
+          type="email"
+          placeholder="E-Posta"
+          onChange={(email) => setEmail(email.target.value)}
+        />
+        <br />
+        <input
+          className="pass"
+          type="password"
+          placeholder="Şifre"
+          onChange={(password) => setPassword(password.target.value)}
+        />
+        <br />
+        <button
+          onClick={() => {
+            setUser(CheckUser(email, password).then((result) => result.data));
+          }}
+        >
+          Giriş Yap
+        </button>
+        <button className="regis">Kayıt Ol</button>
       </main>
     </div>
   );
 }
-
 export default App;

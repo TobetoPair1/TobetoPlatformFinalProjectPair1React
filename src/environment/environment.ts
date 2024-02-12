8a0e17cb466bdf38fbe5ab1dad870740903f89b6
Mehmet Albayrak <albayrakmehmetdeu@gmail.com>
2024-02-12 23:23:18 +0300

    BaseUrl is configurated

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,1 +1,13 @@
-export const BASE_API_URL = "http://localhost:38106/api/";
+let BASE_API_URL: string;
+
+const isWindows = () => {
+    return navigator.appVersion.indexOf("Win") !== -1;
+}
+
+if (isWindows()) {
+    BASE_API_URL = "http://localhost:38106/api/";  
+} else {
+    BASE_API_URL = "http://localhost:5110/api/";  
+}
+
+export { BASE_API_URL };

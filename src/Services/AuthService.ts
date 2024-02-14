c7bb863b25d5f2b6d14c7bb48bd85798808fe87d
Furkan Koçoğlu <furkankocoglu@outlook.com.tr>
2024-02-14 16:42:59 +0300

    ResetPassword & ForgotPassword

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,11 +1,13 @@
 import { AxiosResponse } from 'axios';
 import { LoginRequestModel } from '../Models/Requests/Auth/LoginRequestModel';
-import { RegisterRequestModel} from '../Models/Requests/Auth/RegisterRequestModel';
+import { RegisterRequestModel } from '../Models/Requests/Auth/RegisterRequestModel';
 import { TokenModel } from '../Models/Responses/Token/TokenModel';
 import axiosInstance from '../core/interceptors/axiosInceptor';
+import { ChangePasswordModel } from '../Models/Requests/Auth/ChangePasswordRequestModel';
 
 const login = 'Auths/login';
 const register = 'Auths/register';
+const changepassword = 'Auths/changepassword';
 
 class AuthService {
   login(loginRequest: LoginRequestModel): Promise<AxiosResponse<TokenModel, any>> {
@@ -16,6 +18,10 @@ class AuthService {
     registerRequest: RegisterRequestModel
   ): Promise<AxiosResponse<TokenModel, any>> {
     return axiosInstance.post(register, registerRequest);
+  }
+  changePassword(
+    changePasswordRequest: ChangePasswordModel): Promise<AxiosResponse<boolean, any>> {
+    return axiosInstance.post(changepassword, changePasswordRequest);
   }
 }
 
